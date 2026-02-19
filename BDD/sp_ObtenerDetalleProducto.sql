1792e4649f32cf932b011d14f3789c168ea8a854
Ariany Lopez <arianylopez341@gmail.com>
2026-02-18 21:59:56 -0400

    Eliminacion

--- BDD/sp_ObtenerDetalleProducto.sql
+++
@@ -1,9 +0,0 @@
--- Detalle de Producto - HU2
-CREATE PROCEDURE sp_ObtenerDetalleProducto
-	@ProductoID INT
-AS BEGIN
-	SELECT 
-		ProductoID, Nombre, Descripcion, Precio, StockActual, ImagenURL
-	FROM Productos
-	WHERE ProductoID = @ProductoID;
-END;
